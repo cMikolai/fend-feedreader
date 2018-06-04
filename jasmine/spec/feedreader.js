f2d0e5a3a1aa36c28b01804347bfa7f2f085b1f7
Christin Mikolai <mail@christinmikolai.de>
2018-06-04 21:10:13 +0200

    feat: Adds RSS feeds tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,12 +31,24 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('has URLs defined', function() {
+           allFeeds.forEach((allFeeds) => {
+             expect(allFeeds.url).toBeDefined();
+             expect(allFeeds.url.length).not.toBe(0);
+           });
+         });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('has names defined', function() {
+           allFeeds.forEach((allFeeds) => {
+             expect(allFeeds.name).toBeDefined();
+             expect(allFeeds.name.length).not.toBe(0);
+           });
+         });
     });
 
 
